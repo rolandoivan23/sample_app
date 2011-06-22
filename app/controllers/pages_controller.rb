6e725e9a6084c17c3bb3688a1556e7f6940c5565
Rolando Ivan Vazquez Hernandez <rolando.vazquez.23@gmail.com>
2011-06-22 13:03:13 -0600

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+  	@titulo = "Home"
   end
 
   def contact
+  	@titulo = "Contact"
+  end
+  
+  def about
+  	@titulo = "About"
   end
 
 end
